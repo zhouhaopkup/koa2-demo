fad093cc9959eda1d2402e565e47c0f1d470d137
Devin <18190816904@163.com>
2022-05-31 10:27:19 +0800

    ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,16 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    msg: 'Hello World!',
+    name: 'zhangsan',
+    isMe: true,
+    blogList: [
+      {id: 1, title: 'aaa'}, 
+      {id: 2, title: 'bbb'}, 
+      {id: 3, title: 'ccc'},
+      {id: 4, title: 'ddd'}
+    ]
   })
 })
 
