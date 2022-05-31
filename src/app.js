7716ef39b41bd889cbd7afb253952ed7e71e31a0
Devin <18190816904@163.com>
2022-05-31 09:14:56 +0800

    refactor

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.use(bodyparser({
 }))
 app.use(json())
 app.use(logger())
+// http://localhost:3000/stylesheets/style.css
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
@@ -25,12 +26,12 @@ app.use(views(__dirname + '/views', {
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
