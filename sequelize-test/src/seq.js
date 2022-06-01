6f7323503bd6770c62834303e45c426e9c5f5faa
Devin <18190816904@163.com>
2022-06-01 08:05:35 +0800

    线上环境使用连接池

--- sequelize-test/src/seq.js
+++ sequelize-test/src/seq.js
@@ -5,6 +5,13 @@ const conf = {
     dialect: 'mysql'
 }
 
+// // 线上环境使用连接池
+// conf.pool = {
+//     max: 5, // 连接池中最大的连接数量
+//     min: 0, // 最小
+//     idle: 10000 // 如果一个连接池 10s 之内没有被使用，则释放
+// }
+
 const seq = new Sequelize('koa2_weibo_db', 'root', 'cx269851', conf)
 
 module.exports = seq
